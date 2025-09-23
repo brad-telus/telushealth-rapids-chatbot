a0aedb3a8893e463501a00af4255e936d7d83660
brad-telus <brad.jobe@telus.com>
2025-09-23 11:17:40 -0400

    add POSTGRES_URL

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Lacework agent: start the agent in the background
-if [ -f /var/lib/lacework/datacollector ]; then
-    echo "Starting Lacework agent..."
-    /var/lib/lacework/datacollector &
-else
-    echo "Warning: Lacework datacollector not found"
-fi
+echo "contents of image /"
+ls -la /
+
+echo "contents of image /app"
+ls -la /app
+
+echo "contents of image /app/.next"
+ls -la /app/.next
 
 cd /app
 
